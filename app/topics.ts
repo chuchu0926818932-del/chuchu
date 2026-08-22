9b0d8b498099fb635ec2676a6c31b5d9476a7769
chuchu0926818932-del <228426262+chuchu0926818932-del@users.noreply.github.com>
2026-08-22 10:29:46 +0800

    feat: add 2026-08-22 daily video topics

--- app/topics.ts
+++ app/topics.ts
@@ -14,6 +14,7 @@ import { dailyTopics20260817 } from "./daily-topics-2026-08-17";
 import { dailyTopics20260818 } from "./daily-topics-2026-08-18";
 import { dailyTopics20260819 } from "./daily-topics-2026-08-19";
 import { dailyTopics20260820 } from "./daily-topics-2026-08-20";
+import { dailyTopics20260822 } from "./daily-topics-2026-08-22";
 import { foundationalTopics } from "./foundational-topics";
 
 export type Topic = {
@@ -240,7 +241,7 @@ const legacyTopics: Topic[] = seeds.map((seed, index) => makeTopic(seed, index, 
 // available in the library, but do not move the continuation unlock goal.
 export const firstBatchTopics: Topic[] = legacyTopics;
 export const newTopicCategories = ["女性成長", "金錢價值觀", "親子關係"] as const;
-export const topics: Topic[] = [...firstBatchTopics, ...dailyTopics, ...dailyTopics20260717, ...dailyTopics20260725, ...dailyTopics20260803, ...dailyTopics20260804, ...dailyTopics20260805, ...dailyTopics20260807, ...dailyTopics20260808, ...dailyTopics20260809, ...dailyTopics20260810, ...dailyTopics20260813, ...dailyTopics20260816, ...dailyTopics20260817, ...dailyTopics20260818, ...dailyTopics20260819, ...dailyTopics20260820, ...foundationalTopics];
+export const topics: Topic[] = [...firstBatchTopics, ...dailyTopics, ...dailyTopics20260717, ...dailyTopics20260725, ...dailyTopics20260803, ...dailyTopics20260804, ...dailyTopics20260805, ...dailyTopics20260807, ...dailyTopics20260808, ...dailyTopics20260809, ...dailyTopics20260810, ...dailyTopics20260813, ...dailyTopics20260816, ...dailyTopics20260817, ...dailyTopics20260818, ...dailyTopics20260819, ...dailyTopics20260820, ...dailyTopics20260822, ...foundationalTopics];
 export const firstBatchTopicIds = new Set(firstBatchTopics.map((topic) => topic.id));
 export const sharedTopicIds = new Set(topics.map((topic) => topic.id));
 
